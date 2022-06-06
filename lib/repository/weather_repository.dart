52521f71d618f2fdb1a8d9dc090c00f789ea215f
haterain0203 <haterain0203@gmail.com>
2022-06-06 11:51:45 +0900

    WeatherRepositoryの修正

--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -1,21 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_and_clothes_2/state/weather_state.dart';
+
 class WeatherRepository {
 
   //郵便番号から住所を取得する処理
-  Future<String> getAddress(String zipCode) async {
+  Future<Map<String, dynamic>> getAddress(String zipCode) async {
     //引数の郵便番号をもとにAPI用のURLを生成
     String url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=$zipCode";
 
     //生成したURLでAPIを叩き、住所を取得
     try {
-      var result = await get(Uri.parse(url));
+      var result = await http.get(Uri.parse(url));
       Map<String, dynamic> data = jsonDecode(result.body);
-      final prefecture = data["results"][0]["address1"];
-      final city = data["results"][0]["address2"];
-      final address = prefecture + " " + city;
-      return address;
+      return data;
     } catch(e) {
       print(e);
-      return "エラー";
+      return {};
     }
   }
 
