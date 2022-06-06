5fec707237e1f351ba9a6e9313d81acbf42acc11
haterain0203 <haterain0203@gmail.com>
2022-06-06 21:44:41 +0900

    repository修正

--- lib/notifier/weather_notifier.dart
+++ lib/notifier/weather_notifier.dart
@@ -8,18 +8,18 @@ final weatherFutureProvider = FutureProvider<WeatherState>(((ref) async {
   final repository = ref.read(weatherRepositoryProvider);
   //TODO ローカルに保存されている郵便番号を用いる形に修正
   final address = await repository.getAddress("1000001");
-  final weather = await repository.getWeather("1000001");
-  final yesterdayWeather = await repository.getYesterdayWeather(weather["coord"]["lat"], weather["coord"]["lon"]);
+  final weatherDescAndIcon = await repository.getWeatherDescAndIcon("1000001");
+  final weather = await repository.getWeather(weatherDescAndIcon["coord"]["lat"], weatherDescAndIcon["coord"]["lon"]);
   //TODO ローカルに保存されている郵便番号を用いる形に修正
   final weatherState = WeatherState(
     postalCode: "1000001",
     prefecture: address["results"][0]["address1"],
     city: address["results"][0]["address2"],
-    currentTemperature: (weather["main"]["temp"]).toInt(),
-    maxTemperature: (weather["main"]["temp_max"]).toInt(),
-    weather: weather["weather"][0]["description"],
-    iconStr: weather["weather"][0]["icon"],
-    yesterdayMaxTemperature: (yesterdayWeather["daily"]["temperature_2m_max"][0]).toInt(),
+    currentTemperature: (weatherDescAndIcon["main"]["temp"]).toInt(),
+    maxTemperature: (weather["daily"]["apparent_temperature_max"][1]).toInt(),
+    weather: weatherDescAndIcon["weather"][0]["description"],
+    iconStr: weatherDescAndIcon["weather"][0]["icon"],
+    yesterdayMaxTemperature: (weather["daily"]["apparent_temperature_max"][0]).toInt(),
   );
   return weatherState;
 }));
--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -21,8 +21,8 @@ class WeatherRepository {
     }
   }
 
-  // 郵便番号から天気を取得
-  Future<Map<String, dynamic>> getWeather(String zipCode) async {
+  // 郵便番号から天気descriptionと天気アイコンの取得
+  Future<Map<String, dynamic>> getWeatherDescAndIcon(String zipCode) async {
     var _zipCode = "";
     if(zipCode.contains("-")){
       _zipCode = zipCode;
@@ -40,10 +40,11 @@ class WeatherRepository {
     }
   }
 
-  Future<Map<String, dynamic>> getYesterdayWeather(double lat, double lon) async {
+  // 緯度経度から最高気温と時間別最高気温を取得
+  Future<Map<String, dynamic>> getWeather(double lat, double lon) async {
     // OpenWeatherだと過去のデータが取得できない（有料）のため、過去データはOpenMeteoで取得
     //TODO 統一すべきか？
-    var url = "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&daily=temperature_2m_max&timezone=Asia%2FTokyo&past_days=1";
+    var url = "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&hourly=apparent_temperature&daily=apparent_temperature_max&timezone=Asia%2FTokyo&past_days=1";
     try {
       var result = await http.get(Uri.parse(url));
       Map<String, dynamic> data = jsonDecode(result.body);
