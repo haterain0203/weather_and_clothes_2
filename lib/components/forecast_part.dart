4f13e48e728473a40af0edba1665f3858a36820a
haterain0203 <haterain0203@gmail.com>
2022-06-17 19:50:45 +0900

    不要行の削除

--- lib/components/forecast_part.dart
+++ lib/components/forecast_part.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:sizer/sizer.dart';
 import 'package:weather_and_clothes_2/components/rounded_corner_container.dart';
-import 'package:weather_and_clothes_2/components/weather_info_part.dart';
 import 'package:weather_and_clothes_2/state/weather_state.dart';
 
 import 'clothes_container.dart';
